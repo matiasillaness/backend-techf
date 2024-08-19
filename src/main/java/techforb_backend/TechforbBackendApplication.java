5fbf9e978c7e0a54917fdde7aa6ce942f76d8138
matiasillaness <illanesmatias04@gmail.com>
2024-08-18 22:38:39 -0300

    create project

---
+++ src/main/java/techforb_backend/TechforbBackendApplication.java
@@ -0,0 +1,13 @@
+package techforb_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechforbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechforbBackendApplication.class, args);
+	}
+
+}
